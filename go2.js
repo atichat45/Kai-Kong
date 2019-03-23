f53a9118a75599f838948fe005daf5d2bbe74b36
llarin <44917678+llarin@users.noreply.github.com>
2019-03-23 19:11:11 +0700

    Add files via upload

---
+++ go2.js
@@ -0,0 +1,12 @@
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
+client
+    .logoDetection('thing2.jpg')
+    .then(results => {
+        const labels = results[0].logoAnnotations;
+        console.log('Labels:');
+        labels.forEach(label => console.log(label.description));
+    })
+    .catch(err => {
+        console.error('ERROR:', err)
+    });
